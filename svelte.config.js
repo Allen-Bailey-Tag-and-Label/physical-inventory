bcf5bd5db3a585f5a5b152fc7598922631fa4406
bobthered <bobmcaleavey@gmail.com>
2023-01-19 17:08:08 -0500

    feat: change build folder to public

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      out: 'public'
+    })
   },
 
   preprocess: [
