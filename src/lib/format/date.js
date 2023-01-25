10a0dbbef5189eac902c4af13597f26cc610ff33
Bob McAleavey <bobmcaleavey@gmail.com>
2023-01-25 10:42:34 -0500

    fix: date format error with hours modulus

--- src/lib/format/date.js
+++ src/lib/format/date.js
@@ -1,13 +1,7 @@
 export default (v) => {
   const date = new Date(v);
 
-  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().slice(-2)} ${(
-    date.getHours() +
-    (1 % 12) -
-    1
-  )
-    .toString()
-    .padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} ${
-    date.getHours() >= 12 ? 'PM' : 'AM'
-  }`;
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().slice(-2)} ${
+    ((date.getHours() - 1) % 12) + 1
+  }:${date.getMinutes().toString().padStart(2, '0')} ${date.getHours() >= 12 ? 'PM' : 'AM'}`;
 };
