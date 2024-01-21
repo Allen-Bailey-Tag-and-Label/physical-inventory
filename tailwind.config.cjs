f411794c7923ba60c06c82820bf5ab30d464a118
Bob McAleavey <bobmcaleavey@gmail.com>
2024-01-21 10:35:22 -0500

    feat(sveltewind): add sveltewind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
+const { tailwindcssPaletteGenerator } = require('@bobthered/tailwindcss-palette-generator');
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: tailwindcssPaletteGenerator({
+				colors: ['#4b0cdf', '#706783'],
+				names: ['violet', 'slate']
+			})
+		}
 	},
 
 	plugins: []
