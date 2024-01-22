1dbd5cf54630e0655ae7d3fa2e0f5de1cab78a53
Bob McAleavey <bobmcaleavey@gmail.com>
2024-01-22 12:32:52 -0500

    feat(Pwa): add pwa components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { Pwa, Title } from 'sveltewind/components';
 	import { theme } from '$stores';
 	import '../app.pcss';
 </script>
 
+<Pwa />
+<Title base="Physical Inventory" />
 <slot />
