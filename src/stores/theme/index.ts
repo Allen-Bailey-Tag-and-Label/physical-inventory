d72c431e63e62acdecc720aa2bbac9e88dc33f78
Bob McAleavey <bobmcaleavey@gmail.com>
2024-01-23 14:47:30 -0500

    feat(buttonTransparent): add text color

--- src/stores/theme/index.ts
+++ src/stores/theme/index.ts
@@ -7,7 +7,7 @@ theme.set(sveltewind);
 theme.set({
 	buttonIcon: 'px-3',
 	buttonTransparent:
-		'bg-transparent focus:bg-transparent hover:bg-transparent dark:bg-transparent dark:focus:bg-transparent dark:hover:bg-transparent',
+		'text-current bg-transparent focus:bg-transparent hover:bg-transparent dark:bg-transparent dark:focus:bg-transparent dark:hover:bg-transparent',
 	select: 'py-3',
 	tr: 'even:bg-black/5 dark:even:bg-white/[.025]'
 });
