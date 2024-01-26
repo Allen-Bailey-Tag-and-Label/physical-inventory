329044737130cfdebe644263a07f1261d01bfa45
Bob McAleavey <bobmcaleavey@gmail.com>
2024-01-26 07:52:19 -0500

    feat(getItems): add getItems utility

--- src/routes/(authenticated)/itemNumber/[itemNumber]/+page.server.ts
+++ src/routes/(authenticated)/itemNumber/[itemNumber]/+page.server.ts
@@ -1,5 +1,6 @@
 import { INVENTORY_DATE } from '$env/static/private';
 import { prisma } from '$lib/prisma/index.js';
+import { getItems } from '$utilities';
 
 export const load = async ({ params }) => {
 	const { itemNumber } = params;
@@ -10,64 +11,7 @@ export const load = async ({ params }) => {
 			include: { userCount: true, userVerify: true }
 		})
 	]);
-	const { fg: fgText, raw: rawText } = onHand;
-	const fg = fgText
-		.split('\r\n')
-		.map((item) => item.split('\t'))
-		// .filter((item) => item[3] !== '0')
-		.map((item) => {
-			const itemNumber = item[0];
-			const description = item[1];
-			const cost07 = parseFloat(item[11]);
-			const preCount = +item[15];
-			const preValuation = preCount * cost07;
-			const itemNumberTickets = [...tickets].filter((ticket) => ticket.itemNumber === itemNumber);
-			const afterCount = itemNumberTickets.reduce((total, ticket) => (total += ticket.count), 0);
-			const afterValuation = afterCount * cost07;
-			const deltaCount = afterCount - preCount;
-			const deltaValuation = afterValuation - preValuation;
-			return {
-				itemNumber,
-				description,
-				cost07,
-				preCount,
-				preValuation,
-				itemNumberTicketsCount: itemNumberTickets.length,
-				afterCount,
-				afterValuation,
-				deltaCount,
-				deltaValuation
-			};
-		});
-	const raw = rawText
-		.split('\r\n')
-		.map((item) => item.split('\t'))
-		// .filter((item) => item[6] !== '0')
-		.map((item) => {
-			const itemNumber = item[0];
-			const description = item[1];
-			const cost07 = parseFloat(item[5]);
-			const preCount = +item[6];
-			const preValuation = preCount * cost07;
-			const itemNumberTickets = [...tickets].filter((ticket) => ticket.itemNumber === itemNumber);
-			const afterCount = itemNumberTickets.reduce((total, ticket) => (total += ticket.count), 0);
-			const afterValuation = afterCount * cost07;
-			const deltaCount = afterCount - preCount;
-			const deltaValuation = afterValuation - preValuation;
-			return {
-				itemNumber,
-				description,
-				cost07,
-				preCount,
-				preValuation,
-				itemNumberTicketsCount: itemNumberTickets.length,
-				afterCount,
-				afterValuation,
-				deltaCount,
-				deltaValuation
-			};
-		});
-	const items = [...fg, ...raw];
+	const items = getItems(onHand, tickets);
 	const item = items.find((obj) => obj.itemNumber === itemNumber);
 
 	return { item, tickets };
