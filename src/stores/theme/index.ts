cf00df8355b87180bb16b0dc4546730088401efb
Bob McAleavey <bobmcaleavey@gmail.com>
2025-01-30 09:30:08 -0500

    fix: checkbox color for light theme

--- src/stores/theme/index.ts
+++ src/stores/theme/index.ts
@@ -8,6 +8,7 @@ theme.set({
 	buttonIcon: 'px-3',
 	buttonTransparent:
 		'text-current bg-transparent focus:bg-transparent hover:bg-transparent dark:bg-transparent dark:focus:bg-transparent dark:hover:bg-transparent',
+	checkbox: 'text-white',
 	select: 'py-3',
 	tr: 'even:bg-black/5 dark:even:bg-white/[.025]'
 });
