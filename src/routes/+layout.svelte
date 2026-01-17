22ef05984d99609e07680f0e49479640f252a5a0
Bob McAleavey <bobmcaleavey@gmail.com>
2026-01-17 15:16:13 -0500

    fix: add head links and meta for PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,19 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
+	<link rel="alternate icon" href="/icons/icon-16x16.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-apple-touch.png" />
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="theme-color" content="#09090C" />
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover"
+	/>
+	<title>Physical Inventory</title>
+</svelte:head>
+
 {@render children()}
