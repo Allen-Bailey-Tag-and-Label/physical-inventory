bcab178512bdc50e02c5bf47fbb770cb436e0fa0
Bob McAleavey <bobmcaleavey@gmail.com>
2026-01-17 15:08:36 -0500

    fix: remove hooks

--- src/lib/db/models/UserProfile.ts
+++ src/lib/db/models/UserProfile.ts
@@ -1,18 +1,10 @@
-import { Types } from 'mongoose';
 import { defineModel } from '../defineModel';
-import { hooks } from '../hooks';
 
-export const UserProfile = defineModel(
-	'UserProfile',
-	{
-		ennisId: { type: Number, default: 0 },
-		email: { type: String, default: '' },
-		firstName: { type: String, default: '' },
-		hireDate: { type: Date },
-		lastName: { type: String, default: '' },
-		phone: { type: Number, default: 0 }
-	},
-	{
-		customHooks: [hooks.userProfile._userId, hooks.userProfile.fullName]
-	}
-);
+export const UserProfile = defineModel('UserProfile', {
+	ennisId: { type: Number, default: 0 },
+	email: { type: String, default: '' },
+	firstName: { type: String, default: '' },
+	hireDate: { type: Date },
+	lastName: { type: String, default: '' },
+	phone: { type: Number, default: 0 }
+});
--- src/lib/db/models/index.ts
+++ src/lib/db/models/index.ts
@@ -4,4 +4,3 @@ export { KeyValue } from './KeyValue';
 export { User } from './User';
 export { UserPasswordReset } from './UserPasswordReset';
 export { UserProfile } from './UserProfile';
-export { UserSettings } from './UserSettings';
